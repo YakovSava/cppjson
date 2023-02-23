75823dce39ff94755eed4c8bca35376a3a041f7d
Jacob Savel'ev <g6h6m238929@gmail.com>
2023-02-23 11:00:46 +0500

    Version 0.3
    Alpha version
    
    Added loaders

--- loader.cxx
+++ loader.cxx
@@ -1,1 +1,10 @@
-Режим вывода команд на экран (ECHO) отключен.
+# include <regex>
+# include <string>
+# include "types.hpp"
+using namespace std;
+
+string load(string toPythonDictionary) {
+	toPythonDictionary = regex_replace(toPythonDictionary, regex(JSONObjects::null), PythonObjects::null);
+	toPythonDictionary = regex_replace(toPythonDictionary, regex(JSONObjects::apostrof), PythonObjects::apostrof);
+	return toPythonDictionary;
+}
